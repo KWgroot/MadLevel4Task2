284a39c96c94093d0585d16be6ea73a151f9a5f9
Koen Groot <kwgroot@gmail.com>
2020-10-16 16:43:37 +0200

    All background work considering the database SHOULD now be done, this leaves the biggest task of actually creating the application.

--- app/src/main/java/com/example/madlevel4task2/GameRepository.kt
+++ app/src/main/java/com/example/madlevel4task2/GameRepository.kt
@@ -1,4 +1,25 @@
 package com.example.madlevel4task2
 
-class GameRepository {
+import android.content.Context
+
+class GameRepository(context: Context) {
+
+    private val gameDao: GameDao
+
+    init {
+        val database = GameRoomDatabase.getDatabase(context)
+        gameDao = database!!.gameDao()
+    }
+
+    suspend fun getAllGames(): List<Game> {
+        return gameDao.getAllGames()
+    }
+
+    suspend fun insertGame(game: Game) {
+        gameDao.insertGame(game)
+    }
+
+    suspend fun deleteAllGames() {
+        gameDao.deleteAllGames()
+    }
 }
