b1064ce1e638b7f2482ce77c2795d232a3663d80
Koen Groot <kwgroot@gmail.com>
2020-10-16 16:23:48 +0200

    Started setting up the database table and how they will work with the data access object.

--- app/src/main/java/com/example/madlevel4task2/Game.kt
+++ app/src/main/java/com/example/madlevel4task2/Game.kt
@@ -1,4 +1,28 @@
 package com.example.madlevel4task2
 
-class Game {
-}
+import androidx.annotation.DrawableRes
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import java.util.*
+
+@Entity(tableName = "gameTable")
+data class Game (
+    @DrawableRes
+    @ColumnInfo(name = "player")
+    var user: Int,
+
+    @DrawableRes
+    @ColumnInfo(name = "computer")
+    var computer: Int,
+
+    @ColumnInfo(name = "date")
+    var date: Date,
+
+    @ColumnInfo(name = "result")
+    var result: String,
+
+    @PrimaryKey(autoGenerate = true)
+    @ColumnInfo(name = "id")
+    var id: Long? = null
+)
--- app/src/main/java/com/example/madlevel4task2/RoomDao.kt
+++ app/src/main/java/com/example/madlevel4task2/RoomDao.kt
@@ -1,4 +1,17 @@
 package com.example.madlevel4task2
 
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.Query
+
+@Dao
 interface RoomDao {
+    @Query("SELECT * FROM gameTable")
+    suspend fun getAllGames(): List<Game>
+
+    @Insert
+    suspend fun insertGame(game: Game)
+
+    @Query("DELETE FROM gameTable")
+    suspend fun deleteAllGames()
 }
